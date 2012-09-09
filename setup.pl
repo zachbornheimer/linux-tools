ebc65d4c540143e92d8b3c5dfeebc40660edc8b1
Z. Bornheimer <z@chary.me>
2012-09-09 20:33:44 +0000

    Fixed bugs relating to chrooting and general pretesting bugs.

--- setup.pl
+++ setup.pl
@@ -17,17 +17,21 @@
 # Example:
 # perl setup.pl ext4:root:/dev/sda1 swap:/dev/sda2 ext4:/dev/sda3 timezone:America/New_York nohwclocktoutc
 
-print "You have given the following arguments: ";
+print "You have given the following arguments:\n";
 foreach (@ARGV) {
     print "\t$_\n";
 }
 print "This will now make your system (including formatting your system!) Proceed? ";
 chomp(my $answer = <STDIN>);
-exit if ($answer =~ /^y/i);
+exit if ($answer !~ /^y/i);
 
 print "Proceeding...\n";
 
+print "Setting the default shell to bash...\n";
+$ENV{SHELL}='/bin/bash';
+
 MAKE_FILESYSTEMS: {
+    local @ARGV = @ARGV;
     print "Making the filesystems...\n";
     foreach (@ARGV) {
         if (/^ext4:/) {
@@ -38,17 +42,17 @@ MAKE_FILESYSTEMS: {
             system('mkswap ' . $_ . '; swapon ' . $_);
         }
     }
-    print "Filesystems made.";
+    print "Filesystems made.\n";
 }
 
 MOUNT_FILESYSTEMS: {
     print "Mounting the filesystems...\n";
+    local @ARGV = @ARGV;
     foreach (@ARGV) {
         if (/^ext4:root:/) {
             s/^.*://;
             system('mount ' . $_ . ' /mnt');
-        }
-        if (/^ext4:/) {
+        } elsif (/^ext4:/) {
             s/^ext4://;
             my $sub = $_;
             s/^.*://;
@@ -76,13 +80,16 @@ FSTAB: {
     }
 }
 
-CHROOT: {
-    print "Chrooting...\n";
-    system('arch-chroot /mnt');
-}
+if (fork()) {
+	wait;
+} else {
+    CHROOT: {
+        print "Chrooting...\n";
+        chroot("/mnt");
+    }
 
 SET_LOCALE: {
-    print "Setting the Locale...\n"
+    print "Setting the Locale...\n";
     local @ARGV = ('/etc/locale.gen');
     $^I = '';
     while (<>) {
@@ -94,19 +101,34 @@ SET_LOCALE: {
 
 SET_TIMEZONE: {
     print "Setting the timezone...\n";
-    my $setHWClockToUTC = 1;
+    local @ARGV = @ARGV;
     foreach (@ARGV) {
         if (/^timezone:/) {
             s/^timezone://;
-            if (-e /usr/share/zoneinfo/ . $_) {
-                system('ln -s /usr/share/zoneinfo/' . $_ . ' /etc/localtime; echo ' . $_ . ' >/etc/timezone;');
+            if (-e "/usr/share/zoneinfo/" . $_) {
+                system('rm /etc/localtime; ln -s /usr/share/zoneinfo/' . $_ . ' /etc/localtime; echo ' . $_ . ' >/etc/timezone;');
             }
         }
+    }
+}
+    exit;
+}
+print "Finished in the chroot.\n";
+
+SET_HWCLOCK: {
+    my $setHWClockToUTC = 1;
+    print "Checking if we need to setup the hardware clock...";
+    foreach (@ARGV) {
         if (/^nohwclocktoutc/) {
             $setHWClockToUTC = 0;
         }
     }
     if ($setHWClockToUTC) {
+        print "yes.\nSetting the hardware clock to UTC...\n";
         system('hwclock --systohc --localtime');
+    } else {
+        print "nope.\n";
     }
 }
+
+print "Please restart the computer when you are ready to complete the installation.";
